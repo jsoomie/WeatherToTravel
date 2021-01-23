a3ea164e4f308f278933ee9eb57ee4c7a20fcb4a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-23 09:06:27 -0800

    finally got the link to the openweather api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-$(document).ready() {
-    console.log("Hello World");
-    let apiKey = "ecdfc670fd2d6aee4df67883c5412b31";
 
-}
+$(".btn").click(function(e){
+
+    var searchCity = $(".search-bar").val();
+    var apiKEY = "&appid=795602cf096a742cddb6ae84e10eaedd";
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather";
+    var apiUNITS = "&units=imperial";
+    var apiSEARCH = "?q=" + searchCity;
+    var apiLINK = apiURL + apiSEARCH + apiUNITS + apiKEY;
+
+    e.preventDefault();
+
+    $.ajax({
+        type: "GET",
+        url: apiLINK,
+        success: function(data) {
+            console.log(data);
+            console.log(data.wind.speed);
+        }
+    })
+})
+
